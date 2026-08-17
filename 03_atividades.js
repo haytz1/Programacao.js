43ff01bf639a8e99d6b6f47ba147d2ded661dc48
haytz <107813073+haytz1@users.noreply.github.com>
2026-08-17 16:54:40 -0300

    atividades

---
+++ 03_atividades.js
@@ -0,0 +1,134 @@
+// Atividade 0
+
+console.log(" _____\n"+ "|     |\n"+ "|     |\n"+ "|     |\n"+ " -----" )
+
+
+// ------------------------------------------------------------------
+
+//Atividade 1
+
+let cidade = "Sao Carlos"
+let estado = "Sao Paulo"
+console.log("Eu moro em "+cidade+" - "+estado)
+
+// ------------------------------------------------------------------
+
+//Atividade 2
+
+let numero1 =67
+let numero2 = 76
+let soma = numero1 + numero2
+
+console.log("A soma de "+numero1+" e "+numero2+ " é = "+soma)
+
+// ------------------------------------------------------------------
+
+//Atividade 3
+
+let idade = 18
+let futuro = idade + 10
+
+console.log("Olá, você tem "+idade+ " anos e daqui a 10 anos terá "+futuro)
+
+// ------------------------------------------------------------------
+
+//Atividade 4
+
+let numero = 15
+let dobro = numero * 2
+let triplo = numero * 3
+
+console.log("O Dobro de "+numero+" é "+dobro+" e o triplo é "+triplo)
+
+
+// ------------------------------------------------------------------
+
+//Atividade 5
+
+let capadobatman = "Capa Do Batman"
+let valorbat = 59.99
+let espada = "katana"
+let valorkata = 99.99
+soma = valorbat + valorkata
+
+console.log("A "+capadobatman+" esta saindo por "+valorbat+"\n e a katana esta por apenas "+valorkata+"\n o total dos dois vai sai por "+soma)
+
+// ------------------------------------------------------------------
+
+//Atividade 6
+
+let salario6 = 2000
+let aumento = 500
+let desins = 250
+let newsalario = (salario6 + aumento) - desins
+
+console.log("O seu salario atual é: "+salario6+" E o novo salario sera de "+newsalario)
+
+// ------------------------------------------------------------------
+
+//Atividade 7
+
+let mesada = 100
+let gasto = 35
+soma = mesada - gasto
+
+console.log("Joao ganha "+mesada+" de mesada e gastou "+gasto+" ficando com apenas: "+soma)
+
+// ------------------------------------------------------------------
+
+//Ativadade 8
+
+let base = 1.80
+let altura = 2.80
+soma = base * altura
+
+console.log("O valora da base do triangulo é "+base+" e o da altura é "+altura+" a area calculada e de "+soma)
+
+// ------------------------------------------------------------------
+
+// Atividade 9
+
+let celsius = 32
+let fahrenheit = 32 * 1.8
+let kelvin = 273.15
+let cefah = celsius + fahrenheit
+let ceke = celsius + kelvin
+
+console.log("A temperatira de "+celsius+" graus celsius, em fahrenheit fica "+cefah+" e em kelvin fica "+ceke)
+
+// ------------------------------------------------------------------
+
+//Atividade 10
+
+let horario = 12
+let minuto = 60
+let segundos = 3600
+let horamin = horario * minuto
+let horaseg = horario * segundos
+
+console.log(horario+" horas tem "+horamin+" minutos e "+horaseg+" segundos")
+
+// ------------------------------------------------------------------
+
+// Atiivade 11
+
+let moto = 89.99
+let motinha = 12.99
+motinha = moto
+moto = motinha
+
+console.log("O preço de motinha é "+motinha+" e de moto é "+moto)
+
+// ------------------------------------------------------------------
+
+// Atividade 12
+
+let distancia = 100
+let litros = 20
+let preco = 6
+let percorrido = distancia / litros
+let valorgasto = litros * preco
+
+console.log("O carro percorreu "+distancia+" kilometros utilizou "+litros+" litros, o consumo dele foi de "+percorrido+" e gastou com combustivel "+valorgasto)
+
+// ------------------------------------------------------------------
