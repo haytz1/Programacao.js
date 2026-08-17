0711a97c0ce50fa0ccee307667accecb29e8fbef
haytz <107813073+haytz1@users.noreply.github.com>
2026-08-17 14:47:29 -0300

    Aula 3

---
+++ 03_revisao.js
@@ -0,0 +1,33 @@
+/*
+let alugel = 1900.00
+let condominio = 450.00
+let internet = 99.00
+let total = alugel + condominio + internet
+
+console.log("O total e: "+total)
+*/
+
+// ------------------------------------------------------------------
+
+let nome = "Robertinho" 
+let sobrenome = "Coceira"
+let nome_completo = nome +" "+ sobrenome
+
+console.log("Nao de nem agua para o "+nome_completo)
+
+// ------------------------------------------------------------------
+
+let divida = 200000
+
+
+console.log("Voce deve: "+divida+" De Kwanzas para o banco internacional de Angola")
+
+let desconto = 100
+divida = divida - desconto // Adiciona um novo valor na variavel
+
+console.log("Com nosso desconto oferecido a divida ficara: "+divida)
+
+let bonus = 2000
+divida = divida + bonus
+
+console.log("Pelo atraso de quitar a divida iremos adicionar uma quantia de compensaçao pelo tempo e a divida ficara: "+divida)
